3371439ba16a4e495207b675324781931bf95a3e
Aasim Sani <aasimsani05@gmail.com>
2026-01-21 16:20:01 -0500

    Bump version to 1.3.1

--- gwt.plugin.zsh
+++ gwt.plugin.zsh
@@ -23,7 +23,7 @@
 #   gwt --copy-config-dirs serena feature/branch  -> copies ./serena to worktree
 #   gwt --config                                  -> interactive config menu
 
-GWT_VERSION="1.3.0"
+GWT_VERSION="1.3.1"
 GWT_REPO="aasimsani/gwt-zsh"
 
 # Store install directory when sourced (works with all plugin managers)
